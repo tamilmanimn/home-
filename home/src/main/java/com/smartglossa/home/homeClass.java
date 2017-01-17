b673f15daece3d3f11b41cba7f214a15e5c213c1
tamilmani <tamilmani@smartglossa>
2017-01-17 16:11:44 +0530

    commit

--- home/src/main/java/com/smartglossa/home/homeClass.java
+++ home/src/main/java/com/smartglossa/home/homeClass.java
@@ -3,9 +3,8 @@ package com.smartglossa.home;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.SQLException;      
 import java.sql.Statement;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 
